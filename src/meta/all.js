671aed5c7d55cf15518d7681b3b6afed6c32ff2f
Naitik Shah <n@daaku.org>
2010-02-18 14:30:43 -0800

    Meta component that pulls in everything
    Summary:
    
    Task ID: #
    
    Blame Rev:
    
    Reviewed By: wzhu
    
    CC: platform-diffs@lists.facebook.com
    
    Test Plan: analyzed resources and loaded up:
    http://www.naitik.dev575.snc1.facebook.com/assets.php/en_US/all.js
    
    everything is listed in the components (except CSS which is magically
    included at the bottom).
    
    Revert Plan:
    
    Tags:
    
    Database Impact:
    
    Memcache Impact:
    
    Other Notes:
    
    EImportant:
    
    - begin *PUBLIC* platform impact section -
    Bugzilla: #
    - end platform impact -
    
    DiffCamp Revision: 91237

---
+++ src/meta/all.js
@@ -0,0 +1,22 @@
+/**
+ * A meta component which requires everything.
+ *
+ * @provides fb.all
+ * @requires
+ *   fb.api
+ *   fb.auth
+ *   fb.data
+ *   fb.init
+ *   fb.ui
+ *   fb.xfbml.addtowishlist
+ *   fb.xfbml.comments
+ *   fb.xfbml.element
+ *   fb.xfbml.fan
+ *   fb.xfbml.like
+ *   fb.xfbml.livestream
+ *   fb.xfbml.loginbutton
+ *   fb.xfbml.name
+ *   fb.xfbml.profilepic
+ *   fb.xfbml.serverfbml
+ *   fb.xfbml.sharebutton
+ */
