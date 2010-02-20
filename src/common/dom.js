cd16e809fb03bb0f7ea35ce6134f44b7e9a22b68
Naitik Shah <n@daaku.org>
2010-02-19 16:19:18 -0800

    Tests for DOM and getViewPort for future dialog use
    Summary:
    
    Task ID: #
    
    Blame Rev:
    
    Reviewed By: brent
    
    CC: platform-diffs@lists.facebook.com
    
    Test Plan: Ran the test suite.
    
    Revert Plan:
    
    Tags:
    
    Database Impact:
    
    Memcache Impact:
    
    Other Notes:
    
    EImportant:
    
    - begin *PUBLIC* platform impact section -
    Bugzilla: #
    - end platform impact -
    
    DiffCamp Revision: 91698

--- src/common/dom.js
+++ src/common/dom.js
@@ -101,6 +101,7 @@ FB.provide('Dom', {
     }
 
     var style;
+//#JSCOVERAGE_IF
     if (FB.Dom.getBrowserType() != 'ie') {
       style = document.createElement('style');
       style.type = "text/css";
@@ -141,5 +142,23 @@ FB.provide('Dom', {
       }
     }
     return FB.Dom._browserType;
+  },
+
+  /**
+   * Get the viewport info. Contains size and scroll offsets.
+   *
+   * @returns {Object} with the width and height
+   */
+  getViewportInfo: function() {
+    // W3C compliant, or fallback to body
+    var root = (document.documentElement && document.compatMode == 'CSS1Compat')
+      ? document.documentElement
+      : document.body;
+    return {
+      scrollTop  : root.scrollTop,
+      scrollLeft : root.scrollLeft,
+      width      : self.innerWidth  ? self.innerWidth  : root.clientWidth,
+      height     : self.innerHeight ? self.innerHeight : root.clientHeight
+    };
   }
 });
---
+++ tests/js/dom.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright Facebook Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @provides fb.tests.dom
+ * @requires fb.tests.qunit
+ *           fb.dom
+ */
+////////////////////////////////////////////////////////////////////////////////
+module('dom');
+////////////////////////////////////////////////////////////////////////////////
+
+test(
+  'containsCss',
+
+  function() {
+    var el = document.createElement('div');
+    el.className = 'test1';
+    ok(FB.Dom.containsCss(el, 'test1'), 'should have "test1" class');
+    ok(!FB.Dom.containsCss(el, 'test2'), 'should not have "test2" class');
+  }
+);
+
+test(
+  'addCss',
+
+  function() {
+    var el = document.createElement('div');
+    ok(!FB.Dom.containsCss(el, 'test1'), 'should not have "test1" class');
+    FB.Dom.addCss(el, 'test1');
+    ok(FB.Dom.containsCss(el, 'test1'), 'should have "test1" class');
+  }
+);
+
+test(
+  'removeCss',
+
+  function() {
+    var el = document.createElement('div');
+    ok(!FB.Dom.containsCss(el, 'test1'), 'should not have "test1" class');
+    FB.Dom.addCss(el, 'test1');
+    ok(FB.Dom.containsCss(el, 'test1'), 'should have "test1" class');
+    FB.Dom.removeCss(el, 'test1');
+    ok(!FB.Dom.containsCss(el, 'test1'), 'should not have "test1" class');
+  }
+);
+
+test(
+  'addCssRules',
+
+  function() {
+    var id = 'my-css';
+    FB.Dom.addCssRules('#id { color: #fff; }', id);
+    ok(FB.Dom._cssRules[id], 'expect it to be set');
+    FB.Dom.addCssRules('#id { color: #fff; }', id);
+    ok(FB.Dom._cssRules[id], 'expect it to be set');
+  }
+);
